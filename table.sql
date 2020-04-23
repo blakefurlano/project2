089cb0afd0d252d38eb67c0f93ffa1c281a35af8
Robert Timothy Hable Jr <50716803+Rejanold@users.noreply.github.com>
2020-04-23 09:51:28 -0500

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS Restraunts;
-CREATE TABLE Restraunts(
-	RestrauntID SERIAL PRIMARY KEY, 
-	RestrauntName TEXT,
-	RestrauntType TEXT,
-	RestrauntGoogleLink TEXT,
-	RestrauntIMG TEXT,
+CREATE TABLE Restaurants(
+	RestaurantID SERIAL PRIMARY KEY, 
+	RestaurantName TEXT,
+	RestaurantType TEXT,
+	RestaurantGoogleLink TEXT,
+	RestaurantIMG TEXT,
 	is_deleted INT DEFAULT 0
 );
