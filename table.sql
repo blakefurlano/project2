8f40a98cb1b1be49f4f57ff56f3c4f066e09529a
blakefurlano <blake.furlano@gmail.com>
2020-05-16 16:16:21 -0500

    moved this files to the project2-Server repository

--- table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS Restraunts;
-CREATE TABLE Restaurants(
-	RestaurantID SERIAL PRIMARY KEY, 
-	RestaurantName TEXT,
-	RestaurantType TEXT,
-	RestaurantGoogleLink TEXT,
-	RestaurantIMG TEXT,
-	is_deleted INT DEFAULT 0
-);
